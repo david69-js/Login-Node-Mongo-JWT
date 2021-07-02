c40d96b7cc1afac03367cdb65915a884a34a1c23
david69-js <da3909622@gmail.com>
2021-07-02 17:00:45 -0600

    success deploy

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config({ path: 'prod.env' });
 //Mongo connection
 const mongoose = require('mongoose');
 const app = require('./src/app')
-const host = process.env.HOST || '0.0.0.0';
+//const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 3000;
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
@@ -13,7 +13,7 @@ mongoose.Promise = global.Promise;
 mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("The connection to the database is successful");
-        app.listen(port, host, () => {
+        app.listen(port, () => {
             console.log(`Run server in the Port: ${port}`)
         })
 
